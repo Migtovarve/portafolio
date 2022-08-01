da0dd4a6fd429593c8f915808d57c70a120fe7ed
Miguel <migtovarve@gmail.com>
2022-08-01 06:03:28 -0400

    buttons

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({text}) {
      return (   
         <div className="flex space-x-2 justify-center">
             <div>
-                <button type="button" className="inline-block mx-2 my-4 px-6 pt-2.5 pb-2 text-primary font-medium text-xs leading-normal uppercase rounded shadow-md bg-white hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center
+                <button type="button" className="inline-block mx-2 my-4 px-6 pt-2.5 pb-2 text-primary font-medium text-xs leading-normal uppercase rounded shadow-md bg-white hover:bg-primary hover:text-white hover:shadow-lg focus:bg-primary focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center
                 ">
                 <a href="#contactame">
                 {text}
